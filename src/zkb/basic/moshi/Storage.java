b678bb95181a2122af34e90c74b09dfb861b4c04
zhengkb <zkb2668977574@163.com>
2019-03-18 17:36:11 +0800

    Lock of consumeAndproduce

--- src/zkb/basic/moshi/Storage.java
+++ src/zkb/basic/moshi/Storage.java
@@ -6,47 +6,48 @@ public class Storage {
     private final int MAX_SIZE=10;
     private LinkedList<Object> list=new LinkedList<>();
 
-    public void produce(String producer){
-        synchronized (list){
-            while(list.size() == MAX_SIZE){
-                System.out.println("仓库已满，【"+producer+"】：暂时不能执行生产任务！");
-                try{
+    public void produce(String produce) {
+        synchronized (list) {
+            while(list.size()==MAX_SIZE) {
+                System.out.println("已无法再生产新产品，内部已满");
+                try {
                     list.wait();
-                }catch(InterruptedException e){
+                }catch(InterruptedException e) {
                     e.printStackTrace();
                 }
             }
             list.add(new Object());
-            System.out.println("【"+producer+"】：生产了一个产品\t【现存储量为】："+list.size());
+            System.out.println(produce+"，开始生产，现有产品 "+list.size());
             list.notifyAll();
         }
     }
 
-    public void consume(String consumer){
-        synchronized (list){
-            while(list.size()==0){
-                System.out.println("仓库已空，【"+consumer+"】：暂时不能执行消费任务！");
-                try{
+    public void consume(String consumer) {
+        synchronized (list) {
+            while(list.size()==0) {
+                System.out.println("内部已无产品，无法进行消费");
+                try {
                     list.wait();
-                }catch (InterruptedException e){
+                }catch(InterruptedException e) {
                     e.printStackTrace();
                 }
-                list.remove();
-                System.out.println("【"+consumer+"】：消费了一个产品\t【现存储量为】："+list.size());
-                list.notifyAll();
             }
+            list.remove();
+            System.out.println(consumer+"，开始消费，现有产品"+list.size());
+            list.notifyAll();
         }
     }
 
-    public LinkedList<Object> getList(){
-        return list;
-    }
+//	public void setList(LinkedList<Object> list) {
+//		this.list=list;
+//	}
+//
+//	public int getMAX_SIZE() {
+//		return MAX_SIZE;
+//	}
+//
+//	public LinkedList<Object> getList(){
+//		return list;
+//	}
 
-    public void setList(LinkedList<Object> list){
-        this.list=list;
-    }
-
-    public Integer getMAX_SIZE(){
-        return MAX_SIZE;
-    }
 }
--- src/zkb/basic/moshi/Test.java
+++ src/zkb/basic/moshi/Test.java
@@ -1,23 +1,27 @@
 package zkb.basic.moshi;
 
 public class Test {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Storage storage=new Storage();
-        for(int i=1;i<60;i++){
-            int finalI=i;
+        for(int i=1;i<60;i++) {
+            int finali=i;
             new Thread(new Runnable() {
+
                 @Override
                 public void run() {
-                    storage.produce(String.format("生产者%d：",finalI));
+                    storage.produce(String.format("生产者 %d ", finali));
+
                 }
             }).start();
         }
-        for(int i=1;i<40;i++){
-            int finalI=i;
+        for(int i=1;i<60;i++) {
+            int finali=i;
             new Thread(new Runnable() {
+
                 @Override
                 public void run() {
-                    storage.consume(String.format("消费者%d:", finalI));
+                    storage.consume(String.format("消费者 %d ", finali));
+
                 }
             }).start();
         }
